af1c551ae69e66bf7c41e0eef94a48d97b3b0fef
Greg Kotelnikov <96785534+kotelnikovgreg@users.noreply.github.com>
2022-05-15 22:53:13 +0300

    Update MySQL-HW3.sql

--- MySQL-HW3.sql
+++ MySQL-HW3.sql
@@ -22,12 +22,16 @@ CREATE TABLE profiles(
 	gender CHAR(1),
 	hometown VARCHAR(200),
 	birthday DATETIME,
+	photo_id BIGINT UNSIGNED NULL,
 	created_at DATETIME DEFAULT NOW()
 );
 # 1 X M
 #ALTER TABLE profiles ADD CONSTRAINT fk_profiles_user_id;
 #FOREIGN KEY (user_id) REFERENCES users(id);
 
+ALTER TABLE `profiles` 
+ADD CONSTRAINT fk_user_id
+FOREIGN KEY (user_id) REFERENCES vk.users(id);
 
 DROP TABLE IF EXISTS massages;
 CREATE TABLE massages(
@@ -37,8 +41,9 @@ CREATE TABLE massages(
 	body TEXT,
 	created_at DATETIME DEFAULT NOW(),
 	
+	
 	FOREIGN KEY (for_user_id) REFERENCES users(id),
-	FOREIGN KEY (to_user_id) REFERENCES users(id),
+	FOREIGN KEY (to_user_id) REFERENCES users(id)
 );
 
 
@@ -46,7 +51,7 @@ DROP TABLE IF EXISTS friend_requests;
 CREATE TABLE friend_requests(
 	initiator_user_id BIGINT UNSIGNED NOT NULL,
 	target_user_id BIGINT UNSIGNED NOT NULL,
-	status ENUM('requested', 'approved', 'declined', 'unfriended')
+	status ENUM('requested', 'approved', 'declined', 'unfriended'),
 	created_at DATETIME DEFAULT NOW(),
 	updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
 	
@@ -56,8 +61,88 @@ CREATE TABLE friend_requests(
 	CHECK (initiator_user_id != target_user_id)
 );
 
-# M X M
+DROP TABLE IF EXISTS communities;
+CREATE TABLE communities(
+	id SERIAL,
+	name VARCHAR(150),
+	admin_user_id BIGINT UNSIGNED NOT NULL,
+	
+	INDEX communities_name_idx(name),
+	FOREIGN KEY (admin_user_id) REFERENCES users(id)
+);
 
+DROP TABLE IF EXISTS users_communities;
+CREATE TABLE users_communities(
+	user_id BIGINT UNSIGNED NOT NULL,
+	community_id BIGINT UNSIGNED NOT NULL,
+  
+	PRIMARY KEY (user_id, community_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (community_id) REFERENCES communities(id)
+);
 
+DROP TABLE IF EXISTS media_types;
+CREATE TABLE media_types(
+	id SERIAL,
+    name VARCHAR(255), 
+    created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
+);
 
+DROP TABLE IF EXISTS media;
+CREATE TABLE media(
+	id SERIAL,
+    media_type_id BIGINT UNSIGNED NOT NULL,
+    user_id BIGINT UNSIGNED NOT NULL,
+  	body text,
+    filename VARCHAR(255),
+    -- file blob,    	
+    size INT,
+	metadata JSON,
+    created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (media_type_id) REFERENCES media_types(id)
+);
+
+DROP TABLE IF EXISTS likes;
+CREATE TABLE likes(
+	id SERIAL,
+    user_id BIGINT UNSIGNED NOT NULL,
+    media_id BIGINT UNSIGNED NOT NULL,
+    created_at DATETIME DEFAULT NOW()
+);
+
+DROP TABLE IF EXISTS `photo_albums`;
+CREATE TABLE `photo_albums` (
+	`id` SERIAL,
+	`name` varchar(255) DEFAULT NULL,
+    `user_id` BIGINT UNSIGNED DEFAULT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+  	PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `photos`;
+CREATE TABLE `photos` (
+	id SERIAL,
+	`album_id` BIGINT unsigned NULL,
+	`media_id` BIGINT unsigned NOT NULL,
+
+	FOREIGN KEY (album_id) REFERENCES photo_albums(id),
+    FOREIGN KEY (media_id) REFERENCES media(id)
+);
+
+ALTER TABLE vk.likes 
+ADD CONSTRAINT likes_fk 
+FOREIGN KEY (media_id) REFERENCES vk.media(id);
+
+ALTER TABLE vk.likes 
+ADD CONSTRAINT likes_fk_1 
+FOREIGN KEY (user_id) REFERENCES vk.users(id);
+
+ALTER TABLE vk.profiles 
+ADD CONSTRAINT profiles_fk_1 
+FOREIGN KEY (photo_id) REFERENCES vk.media(id);
 
